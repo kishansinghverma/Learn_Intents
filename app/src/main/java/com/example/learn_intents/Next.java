e8bcce426bc609b6d4532e2cf4ff24affb0f3d2f
kishansinghverma <kishansinghverma@gmail.com>
2019-02-25 18:34:59 +0530

    init commit

--- app/src/main/java/com/example/learn_intents/Next.java
+++ app/src/main/java/com/example/learn_intents/Next.java
@@ -1,13 +1,17 @@
 package com.example.learn_intents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Next extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +20,12 @@ public class Next extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        tv=findViewById(R.id.textView);
+        Intent intent=getIntent();
+        String str=intent.getStringExtra("txt");
+
+        tv.setText(str);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
