f2bcf8b9265719bb2b140db8cf89e04542c47245
kishansinghverma <kishansinghverma@gmail.com>
2019-05-05 13:32:10 +0530

    final

--- app/src/main/java/com/example/learn_intents/MainActivity.java
+++ app/src/main/java/com/example/learn_intents/MainActivity.java
@@ -1,33 +1,66 @@
 package com.example.learn_intents;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btn;
-    EditText edt;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn=findViewById(R.id.button);
-        edt=findViewById(R.id.editText);
+        findViewById(R.id.sms).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        btn.setOnClickListener(new View.OnClickListener() {
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("smsto:7037433280"));
+                intent.putExtra("sms_body", "Hi There!!");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.mail).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:kishansinghverma@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Test Subject");
+                intent.putExtra("body", "This is a test Mail!");
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("tel:7037433280"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.web).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent=new Intent(MainActivity.this, Next.class);
-                intent.putExtra("txt", edt.getText().toString());
                 startActivity(intent);
             }
         });
+
+
     }
 }
